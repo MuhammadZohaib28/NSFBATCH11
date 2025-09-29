37b8f2544a031d0821d1cb30187fdd0aa019d814
= <76484677+MuhammadZohaib28@users.noreply.github.com>
2025-09-29 22:03:14 +0500

    JS DAY 1

--- JAVASCRIPT/day1.js
+++ JAVASCRIPT/day1.js
@@ -1,4 +1,45 @@
-var number1 = 90;
-var number2 = 10;
+// SINGLE THREADED LANGUAGE
+// WEBSITES, WEB APP, APPS, IOS, ANDROID, DESKTOP APP ......
 
-console.log(number1 + number2);
+// JS - JAVASCRIPT
+
+// WEBSITE
+
+//DATA TYPES
+// 1 -  STRING  "   "  ANY ALPHA
+// 2 - NUMBER
+
+console.log("Hello Wor"); //PRINTING STATEMENT
+
+console.log("10" + "10" + ""); // CONCATENATION
+console.log(20 + 20, "Yahan par 20 + 20 horaha hai"); // ADDITION
+
+var nayaNumber = 500;
+var dusraNumber = 90;
+
+console.log(nayaNumber, "yeh variabl nayaNumber say araha hai");
+
+console.log(nayaNumber, "YEH REUSE HUA HAI");
+
+console.log(nayaNumber - dusraNumber);
+
+// RE DECLARATION
+var nayaNumber = 10;
+
+console.log(nayaNumber);
+
+// let keyword
+
+let myName = "Faizan";
+
+let MyName = "Zohaib";
+
+console.log(MyName);
+
+// VAR MAI HAM LOG REDECLARATION KAR SAKTE HAIN
+// LET MAI HAM LOG REDECLARATION NAHI KAR SAKTE HAIN
+
+let newNum1 = 100;
+let newNum2 = 200;
+
+console.log("200" + "100");
